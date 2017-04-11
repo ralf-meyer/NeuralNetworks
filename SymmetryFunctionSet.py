eeb878d65d3047549a635455457fc90e356c6067
ralf-meyer <meyer.ralf@yahoo.com>
2017-04-11 16:41:23 +0200

    First runnable commit of the symmetry functions

--- SymmetryFunctionSet.py
+++ SymmetryFunctionSet.py
@@ -1,4 +1,5 @@
-import SymmetryFunctions
+import SymmetryFunctions as SFs
+import numpy as _np
 
 class SymmetryFunctionSet(object):
     
@@ -7,15 +8,56 @@ class SymmetryFunctionSet(object):
         self.cutoff = cutoff
         self.symmetry_functions = {}
         
-    def add_radial_functions(self, etas):
+    def add_radial_functions(self, rss, etas):
         for a in self.atomtypes:
             for b in self.atomtypes:
-                for eta in etas:
-                    if not (a,b) in self.symmetry_functions:
-                        self.symmetry_functions[(a,b)]= [lambda r: 
-                            SymmetryFunctions.radial_function(r, eta, 
-                                                              self.cutoff)]
+                if not (a,b) in self.symmetry_functions:
+                    self.symmetry_functions[(a,b)]= [
+                        SFs.RadialSymmetryFunction(rs, eta, self.cutoff)
+                        for eta in etas for rs in rss]
+                else:
+                    self.symmetry_functions[(a,b)].extend([
+                        SFs.RadialSymmetryFunction(rs, eta, self.cutoff)
+                        for eta in etas for rs in rss])
+                                                              
+    def add_angluar_functions(self, etas, zetas, lambs):
+        for a in self.atomtypes:
+            for b in self.atomtypes:
+                for c in self.atomtypes:
+                    if not (a,b,c) in self.symmetry_functions:
+                        self.symmetry_functions[(a,b,c)]= [
+                            SFs.AngularSymmetryFunction(eta, zeta, lamb, self.cutoff)
+                            for eta in etas for zeta in zetas for lamb in lambs]
                     else:
-                        self.symmetry_functions[(a,b)].append(lambda r: 
-                            SymmetryFunctions.radial_function(r, eta, 
-                                                              self.cutoff))
+                        self.symmetry_functions[(a,b,c)].extend([
+                            SFs.AngularSymmetryFunction(eta, zeta, lamb, self.cutoff)
+                            for eta in etas for zeta in zetas for lamb in lambs])
+    
+    def eval_geometry(self, geometry):
+        out = []        
+        for i in geometry:
+            i_type = i[0]      
+            vals = {}
+            for j in geometry:
+                j_type = j[0]
+                key = (i_type, j_type)
+                if key in self.symmetry_functions:
+                    if not key in vals:
+                        vals[key] = _np.zeros(len(self.symmetry_functions[key]))        
+                    if not i is j:
+                        rij = _np.linalg.norm(i[1]-j[1])
+                        vals[key] += map(lambda f: f(rij), self.symmetry_functions[key])
+                    for k in geometry:
+                        k_type = k[0]
+                        key2 = (i_type, j_type, k_type)
+                        if not key2 in vals:
+                            vals[key2] = _np.zeros(len(self.symmetry_functions[key2]))
+                        if not ((i is j) or (i is k) or (j is k)):
+                            rij = _np.linalg.norm(i[1]-j[1])
+                            rik = _np.linalg.norm(i[1]-k[1])
+                            costheta = _np.dot(i[1]-j[1],i[1]-k[1])/(rij*rik)
+                            vals[key2] += map(lambda f: f(rij, rik, costheta), 
+                                self.symmetry_functions[key2])
+            out.append(vals)            
+            #out.append(_np.array(vals.values()).flatten())
+        return out        
--- SymmetryFunctions.py
+++ SymmetryFunctions.py
@@ -3,9 +3,31 @@ import numpy as _np
 def cutoff_function(r,cut):
     # Weird formulation allows for calls using vectors
     return (r <= cut)*0.5*(_np.cos(_np.pi*r/cut)+1)
-
-def radial_function(r, rs, eta, cut):
-    return _np.exp(-eta*(r-rs)**2)*cutoff_function(r,cut)
     
-def angular_function(r, zeta, cut):
-    pass
+class RadialSymmetryFunction(object):
+    def __init__(self, rs, eta, cut):
+        self.rs = rs
+        self.eta = eta
+        self.cut = cut
+        
+    def __call__(self, r):
+        return _np.exp(-self.eta*(r-self.rs)**2)*cutoff_function(r,self.cut)
+
+class AngularSymmetryFunction(object):
+    def __init__(self, eta, zeta, lamb, cut):
+        self.eta = eta
+        self.zeta = zeta
+        self.lamb = lamb
+        self.cut = cut
+        
+    def __call__(self, rij, rik, costheta):
+        return 2**(1-self.zeta)* ((1 + self.lamb*costheta)**self.zeta * 
+            _np.exp(-self.eta*(rij**2+rik**2)) * cutoff_function(rij,self.cut)*
+            cutoff_function(rik,self.cut))
+
+def radial_function(rij, rs, eta, cut):
+    return _np.exp(-eta*(rij-rs)**2)*cutoff_function(rij,cut)
+    
+def angular_function(rij, rik, costheta, eta, zeta, lamb,  cut):
+    return 2**(1-zeta)* ((1 + lamb*costheta) * _np.exp(-eta*(rij**2+rik**2)) *
+        cutoff_function(rij,cut)*cutoff_function(rik,cut))
