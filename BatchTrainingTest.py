aa509b9ad2f9b04d50ea3eb0f1b41aa58ebb84a4
alexf1991 <alex.fuchs@student.tugraz.at>
2017-04-25 16:00:17 +0200

    Working batch training example

--- BatchTrainingTest.py
+++ BatchTrainingTest.py
@@ -8,37 +8,51 @@ Created on Wed Apr 19 10:34:43 2017
 
 import NeuralNetworkUtilities as NN
 import numpy as np
+import matplotlib.pyplot as plt
 
-NrAu=5
-NrNi=5
+plt.close("all")
+
+NrAu=2
+NrNi=1
 
 Data=NN.DataInstance()
 
-Data.XYZfile="NiAu_data.xyz"
-Data.Logfile="log.2atoms"
+Data.XYZfile="NiAu_data_2AU1Ni.xyz"
+Data.Logfile="log.3atoms"
 Data.SymmFunKeys=["1","2"]
 Data.Rs=np.arange(0.1,7,1).tolist()
-Data.Etas=np.arange(0.1,2,0.5).tolist()
+Data.Etas=np.arange(0.1,1.1,1).tolist()
 Data.Lambs=[1.0,-1.0]
 Data.Zetas=np.arange(0.1,2,0.5).tolist()
 
 Data.read_files()
-Batches=Data.get_data(1000)
+Batches=Data.get_data(100,70)
+ValidationBatches=Data.get_data(100,30)
+#Batches=Data.get_data(0,1,True)
 
 Training=NN.AtomicNeuralNetInstance()
-Training.Structures.append([Data.SizeOfInputs[0],500,1])
-Training.NumberOfSameNetworks.append(1)
-Training.Structures.append([Data.SizeOfInputs[1],500,1])
-Training.NumberOfSameNetworks.append(1)
-Training.make_and_initialize_network()
+Training.Structures.append([Data.SizeOfInputs[0],10,1])
+Training.NumberOfSameNetworks.append(NrNi)
+Training.Structures.append([Data.SizeOfInputs[1],10,1])
+Training.NumberOfSameNetworks.append(NrAu)
+Training.HiddenType="truncated_normal"
+Training.HiddenData=None
+Training.BiasData=None
+Training.ActFun="tanh"
+Training.ActFunParam=None
+Training.LearningRate=0.000001
+Training.Epochs=1000
 Training.MakePlots=True
+Training.OptimizerType="Adam"
+Training.make_and_initialize_network()
+
 #With batches
-Training.Epochs=500
 Training.TrainingBatches=Batches
+Training.ValidationBatches=ValidationBatches
 Training.start_batch_training()
 
 #Witout batches
-#Batches=Data.get_data(0,70,True)
+
 #Training.TrainingInputs=Batches[0][0]
 #Training.TrainingOutputs=Batches[0][1]
 #Training.start_training()
