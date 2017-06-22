613c7db5f5c923aae48e38ba3fd94cbb70d4cf4d
Alexander Fuchs <afuchs@qciep01.tugraz.at>
2017-06-22 18:12:32 +0200

    Update of example

--- BatchTrainingTest.py
+++ BatchTrainingTest.py
@@ -20,10 +20,11 @@ Training=NN.AtomicNeuralNetInstance()
 Training.XYZfile="NiAu_data_2AU1Ni.xyz"
 Training.Logfile="log.3atoms"
 Training.SymmFunKeys=["1","2"]
-Training.Rs=np.arange(0.1,7,1).tolist()
-Training.Etas=np.arange(0.1,1.1,1).tolist()
+Training.NumberOfRadialFunctions=6
+#angular symmetry function settings
 Training.Lambs=[1.0,-1.0]
-Training.Zetas=np.arange(0.1,2,0.5).tolist()
+Training.Zetas=[0.025,0.045,0.075,0.1,0.15,0.2,0.3,0.5,0.7,1,1.5,2,3,5,10,18,36,100]
+Training.Etas=[0.1]
 
 Training.read_files()
 Batches=Training.get_data(100,70)
@@ -31,19 +32,21 @@ ValidationBatches=Training.get_data(100,30)
 #Batches=Data.get_data(0,1,True)
 
 
-Training.Structures.append([Training.SizeOfInputs[0],10,10,1])
+Training.Structures.append([Training.SizeOfInputs[0],25,25,10,1])
 Training.NumberOfSameNetworks.append(NrNi)
-Training.Structures.append([Training.SizeOfInputs[1],10,10,1])
+Training.Structures.append([Training.SizeOfInputs[1],25,25,10,1])
 Training.NumberOfSameNetworks.append(NrAu)
 Training.HiddenType="truncated_normal"
 Training.HiddenData=list()
 Training.BiasData=list()
-Training.ActFun="relu"
+Training.ActFun="elu"
 Training.ActFunParam=None
-Training.LearningRate=0.0001
+Training.LearningRate=0.001
+Training.dE_Criterium=0.043
 Training.Epochs=1500
 Training.MakePlots=True
 Training.OptimizerType="Adam"
+Training.CostFunType="other"
 Training.make_and_initialize_network()
 
 #With batches
