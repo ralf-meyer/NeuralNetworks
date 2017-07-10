88e8d8ae02681dd517b494999ae48d5482976138
Alexander Fuchs <afuchs@qciep01.tugraz.at>
2017-07-10 12:58:01 +0200

    Deprecated setup file

--- cythonize.py
+++ cythonize.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 # compiler_directives={'boundscheck': False, 'wraparound': False}
-
+#deprecated setup file
 setup(
     name = 'SymmetryFuns',
     ext_modules = cythonize(["NeuralNetworks/SymmetryFunctionsC.pyx", "NeuralNetworks/SymmetryFunctionSetC.pyx"]), 
