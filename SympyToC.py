278531c56795ef79c0443e36c6bdfcb18eab5ff5
Alexander Fuchs <afuchs@qciep01.tugraz.at>
2017-08-01 16:51:29 +0200

    Functionalities to add custom symmetry functions and its derivatives to the files

--- SympyToC.py
+++ SympyToC.py
@@ -1,8 +1,272 @@
-
-from sympy import Symbol, cse, ccode
+import re
+from sympy import Symbol, cse, ccode,diff
 from sympy.utilities import numbered_symbols
 
-# Inspired by http://stackoverflow.com/questions/22665990/optimize-code-generated-by-sympy
+def is_variable(symbol,variables_list):
+    out=False
+    if symbol in variables_list:
+        out=True
+    return out
+
+
+def addToSymmetryFunctionSet(symname_list, symfunc_list,variables_list):
+    
+    with open("Git/NeuralNetworks/symmetryFunctionSet.cpp","r") as file:
+        cpp_code=file.read()
+        #find insert position
+        file.close()
+    with open("Git/NeuralNetworks/symmetryFunctionSet.cpp","w+") as file:
+        try:
+            start_add_idx=cpp_code.index("//Start of section for adding custom symmetry functions\n")\
+            +len("//Start of section for adding custom symmetry functions\n")
+            end_add_idx=cpp_code.index("//End of section for adding custom symmetry functions\n")
+            print("symmetryFunctionSet found insert point!")
+        except:
+            #in case file is manually modified
+            semi_idx=[i.start() for i in re.finditer(';', cpp_code)]
+            eval_geom_idx=cpp_code.index("eval_geometry(")
+            for i,idx in enumerate(semi_idx):
+                if idx > eval_geom_idx:
+                    start_add_idx=semi_idx[i-1]+1
+                    temp_code="//Start of section for adding custom symmetry functions\n"
+                    temp_code+="//End of section for adding custom symmetry functions\n"
+                    cpp_code=cpp_code[:start_add_idx]+temp_code+cpp_code[start_add_idx:]
+                    start_add_idx=start_add_idx+len("//Start of section for adding custom symmetry functions\n")
+                    end_add_idx=cpp_code.index("//End of section for adding custom symmetry functions\n")
+                    break 
+        try:
+            start_vec_idx=cpp_code.index("//Start of section custom symmetry functions vector\n")
+            end_vec_idx=cpp_code.index("//End of section custom symmetry functions vector\n")
+        except:
+            semi_idx=[i.start() for i in re.finditer(';', cpp_code)]
+            eval_geom_idx=cpp_code.index("eval_geometry(")
+            for i,idx in enumerate(semi_idx):
+                if idx > eval_geom_idx:
+                    private_idx=cpp_code.index("private:",idx)+len("private:")
+                    start_vec_idx=private_idx+1
+                    end_vec_idx=start_vec_idx
+                    temp_code="//Start of section custom symmetry functions vector\n"
+                    temp_code+="//End of section custom symmetry functions vector\n"
+                    cpp_code=cpp_code[:start_vec_idx]+temp_code+cpp_code[start_vec_idx]
+                    break 
+
+        #remove old custom add functions
+        cpp_code=cpp_code[:start_add_idx]+cpp_code[end_add_idx:]
+        cpp_code=cpp_code[:start_vec_idx]+cpp_code[end_vec_idx:]
+        #insert new code
+
+        for symname,symfunc in zip(symname_list,symfunc_list): 
+            if type(symfunc)!=int and type(symfunc)!=float:
+                #construct code
+                tmpsyms = numbered_symbols("tmp")
+                symbols, simple = cse(symfunc, symbols=tmpsyms)
+                symbolslist = map(lambda x:str(x), list(symfunc.atoms(Symbol)) )
+                symbolslist.sort()
+                varstring=",".join( " double "+x for x in symbolslist if not(is_variable(x,variables_list)))[1:]
+                argstring=",".join(x for x in symbolslist if not(is_variable(x,variables_list)))[1:]
+                temp_code ="\tvoid add_"+str(symname)+"("+varstring+",int cutoff_type) {\n"
+                temp_code +="\t\t"+str(symname)+"SymFuns.push_back("+str(symname)+"SymmetryFunction("+argstring+",cutoff_type));\n"
+                temp_code +="\t};\n"
+                #add to previous
+                cpp_code=cpp_code[:start_add_idx]+temp_code+cpp_code[start_add_idx:]
+            else:
+                cpp_code +="\tvoid add_"+str(symname)+"(int cutoff_type) {\n"
+                cpp_code +="\t\t"+str(symname)+"SymFuns.push_back("+str(symname)+"SymmetryFunction(cutoff_type));\n"
+                cpp_code +="};\n"
+                
+            try:
+                start_vec_idx=cpp_code.index("//Start of section custom symmetry functions vector\n")+len("//Start of section custom symmetry functions vector\n")
+            except:
+                semi_idx=[i.start() for i in re.finditer(';', cpp_code)]
+                eval_geom_idx=cpp_code.index("eval_geometry(")
+                for i,idx in enumerate(semi_idx):
+                    if idx > eval_geom_idx:
+                        private_idx=cpp_code.index("private:",idx)+len("private:")
+                        start_vec_idx=private_idx+1
+                        break 
+            temp_code="\tstd::vector <"+str(symname)+"SymmetryFunction> "+str(symname)+"SymFuns;\n"
+            #add to previous
+            cpp_code=cpp_code[:start_vec_idx]+temp_code+cpp_code[start_vec_idx:]
+        file.write(cpp_code)
+        file.close()
+        
+def addToSymmetryFunctionsHeader(symname_list, symfunc_list,variables_list):
+    
+    with open("Git/NeuralNetworks/symmetryFunctions.h","r") as file:
+        h_code=file.read()
+        file.close()
+        
+    with open("Git/NeuralNetworks/symmetryFunctions.h","w+") as file:
+        #find insert position
+        try:
+            start_idx=h_code.index("//Start of section for custom symmetry functions\n")\
+            +len("//Start of section for custom symmetry functions\n")
+            end_idx=h_code.index("//End of section for custom symmetry functions\n")
+            print("symmetryFunctions header found insert point!")
+        except:
+            #in case file is manually modified
+            semi_idx=[i.start() for i in re.finditer(';', h_code)]
+            ang_symfun_idx=h_code.index("AngularSymmetryFunction")
+            for i,idx in enumerate(semi_idx):
+                if idx > ang_symfun_idx:
+                    start_idx=semi_idx[i]+1
+                    temp_code="//Start of section for custom symmetry functions\n"
+                    temp_code+="//End of section for custom symmetry functions\n"
+                    h_code=h_code[:start_idx]+temp_code+h_code[start_idx:]
+                    start_idx=start_idx+len("//Start of section for custom symmetry functions\n")
+                    end_idx=h_code.index("//End of section for custom symmetry functions\n")
+                    break 
+
+        #remove old custom add functions
+        h_code=h_code[:start_idx]+h_code[end_idx:]
+        #insert new code
+
+        for symname,symfunc in zip(symname_list,symfunc_list): 
+            #construct code
+            tmpsyms = numbered_symbols("tmp")
+            symbols, simple = cse(symfunc, symbols=tmpsyms)
+            symbolslist_str = map(lambda x:str(x), list(symfunc.atoms(Symbol)))
+            symbolslist = map(lambda x:x, list(symfunc.atoms(Symbol)))
+            zipped_list=zip(symbolslist_str,symbolslist)
+            zipped_list.sort(key=lambda t:t[0])
+            symbolslist_str,symbolslist=map(list,zip(*zipped_list))
+            conststring=",".join( " double "+str(x) for x in symbolslist if not(is_variable(x,variables_list)))[1:]
+            temp_code ="class "+str(symname)+"SymmetryFunction:: public Symmetry Function\n"
+            temp_code +="{\n"
+            for symbol in symbolslist:
+                if not(is_variable(symbol,variables_list)):
+                    temp_code +="\t"+str(symbol)+" = "+str(symbol)+"_i;\n"
+            temp_code +="\tCutoffFunction *cutFun\n"
+            temp_code +="\tpublic:\n"
+            temp_code +="\t\t"+str(symname)+"SymmetryFunction("+conststring+");\n"
+            temp_code +="\t\tdouble eval(double* r);\n"
+            for i in range(0,len(variables_list)):
+                    temp_code +="\t\tdouble derivative_"+str(variables_list[i])+"(double* r);\n"
+                    
+            temp_code +="};\n"
+            #add to previous
+            h_code=h_code[:start_idx]+temp_code+h_code[start_idx:]
+            #write new file
+        file.write(h_code)
+        file.close()
+    
+def addToSymmetryFunctions(symname_list, symfunc_list,variables_list):
+    
+    with open("Git/NeuralNetworks/symmetryFunctions.cpp","r") as file:
+        cpp_code=file.read()
+        file.close()
+        
+    with open("Git/NeuralNetworks/symmetryFunctions.cpp","w+") as file:
+        #find insert position
+        try:
+            start_idx=cpp_code.index("//Start of section for custom symmetry functions\n")\
+            +len("//Start of section for custom symmetry functions\n")
+            end_idx=cpp_code.index("//End of section for custom symmetry functions\n")
+            print("symmetryFunctions found insert point!")
+        except:
+            #in case file is manually modified
+            semi_idx=[i.start() for i in re.finditer(';', cpp_code)]
+            ang_symfun_idx=cpp_code.index("AngularSymmetryFunction")
+            for i,idx in enumerate(semi_idx):
+                if idx > ang_symfun_idx:
+                    start_idx=semi_idx[i]+1
+                    temp_code="//Start of section for custom symmetry functions\n"
+                    temp_code+="//End of section for custom symmetry functions\n"
+                    cpp_code=cpp_code[:start_idx]+temp_code+cpp_code[start_idx:]
+                    start_idx=start_idx+len("//Start of section for custom symmetry functions\n")
+                    end_idx=cpp_code.index("//End of section for custom symmetry functions\n")
+                    break 
+
+        #remove old custom add functions
+        cpp_code=cpp_code[:start_idx]+cpp_code[end_idx:]
+        #insert new code
+
+        for symname,symfunc in zip(symname_list,symfunc_list): 
+            #construct code
+            tmpsyms = numbered_symbols("tmp")
+            symbols, simple = cse(symfunc, symbols=tmpsyms)
+            symbolslist_str = map(lambda x:str(x), list(symfunc.atoms(Symbol)))
+            symbolslist = map(lambda x:x, list(symfunc.atoms(Symbol)))
+            zipped_list=zip(symbolslist_str,symbolslist)
+            zipped_list.sort(key=lambda t:t[0])
+            symbolslist_str,symbolslist=map(list,zip(*zipped_list))
+            conststring_i=",".join( " double "+str(x)+"_i" for x in symbolslist if not(is_variable(x,variables_list)))[1:]
+            cutstring="*".join( "cutFun->eval("+str(x)+")" for x in symbolslist if is_variable(x,variables_list) and "costheta" not in str(x))
+            temp_code =str(symname)+"SymmetryFunction::"+str(symname)+"SymmetryFunction("+conststring_i+",int cutoff_type)\n"
+            for symbol in symbolslist:
+                if not(is_variable(symbol,variables_list)):
+                    temp_code +="\t"+str(symbol)+" = "+str(symbol)+"_i;\n"
+            temp_code +="\tif (cutoff_type == 0)\n"
+            temp_code +="\t\tcutFun = new CosCutoffFunction(cutoff);\n"
+            temp_code +="\telse if(cutoff_type == 1)\n"
+            temp_code +="\t\tcutFun = new TanhCutoffFunction(cutoff);\n"
+            temp_code +="\telse\n"
+            temp_code +="\t\tcutFun = 1;\n"
+            temp_code +="};\n"
+            for i in range(0,len(variables_list)+1):
+                if i==0:
+                    temp_code +="double "+str(symname)+"SymmetryFunction::eval(double* r)\n"
+                    fun=symfunc
+                else:
+                    temp_code +="double "+str(symname)+"SymmetryFunction::derivative_"+str(variables_list[i-1])+"(double* r)\n"
+                    fun=diff(symfunc,variables_list[i-1])
+                    tmpsyms = numbered_symbols("tmp")
+                    symbols, simple = cse(symfunc, symbols=tmpsyms)
+                temp_code +="{\n"
+                for s in symbols:
+                    temp_code +=  "\tdouble " +ccode(s[0]) + " = " + ccode(s[1]) + ";\n"
+                if i>0:
+                    if variables_list[i-1] in fun.free_symbols:   
+                        funstr=str(ccode(simple[0]))
+                    else:
+                        funstr="0"
+                else:
+                    funstr=str(ccode(simple[0]))
+                    
+                if "Heaviside" in funstr:
+                    #get argument of Heaviside function
+                    heavi_idx=funstr.index("Heaviside")
+                    arg_start=funstr.index("(",heavi_idx)
+                    arg_end=funstr.index(")",heavi_idx)
+                    arg=funstr[arg_start+1:arg_end]
+                    temp_code += "\tif(("+ccode(arg)+") < 0) {\n"
+                    temp_code += "\t\treturn = 0.0; }\n"
+                    temp_code += "\telse{"
+                    temp =  "\t\treturn = " + funstr[:heavi_idx-1]+"*"+cutstring+";\n }"
+                    
+                else:
+                    if funstr!="0":
+                        temp =  "\treturn = " + funstr+"*"+cutstring+";\n"
+                    else:
+                        temp =  "\treturn = 0.0;\n"
+                        
+                #replace variables with pointer
+                ct=0
+                for s in symbolslist:
+                    if is_variable(s,variables_list):
+                        temp=temp.replace(str(s),"r["+str(ct)+"]")
+                        ct+=1
+                temp_code += temp
+                temp_code +="};\n"
+
+            #add to previous
+            cpp_code=cpp_code[:start_idx]+temp_code+cpp_code[start_idx:]
+            #write new file
+        file.write(cpp_code)
+        file.close()
+        
+def sympyToCpp(symname_list,symfunc_list,variables_list):
+    
+    if len(symname_list)==len(symfunc_list):
+        addToSymmetryFunctionSet(symname_list, symfunc_list,variables_list)
+        addToSymmetryFunctions(symname_list, symfunc_list,variables_list)
+        addToSymmetryFunctionsHeader(symname_list, symfunc_list,variables_list)
+    else:
+        print("Number of names "+str(len(symname_list))\
+              +" does not match number of symmetry functions "+str(len(symname_list))+" specified!")
+    
+    
+
 def sympyToC(symname, symfunc):
 
     if type(symfunc)!=int and type(symfunc)!=float:
