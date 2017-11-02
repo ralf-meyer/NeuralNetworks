2172e4d98e99a46fbebd307ffd712e6b98e1c3f8
ralf-meyer <meyer.ralf@yahoo.com>
2017-11-02 16:38:08 +0100

    CHG: removed list of tests. No longer necessary.

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,7 +1,5 @@
 import unittest
 
-tests = ['tests.dummy_test.py',]
-
 if __name__ == '__main__':
     testsuite = unittest.TestLoader().discover('.')
     unittest.TextTestRunner(verbosity=1).run(testsuite)
