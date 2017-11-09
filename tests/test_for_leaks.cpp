8719552ede84afcd4abe0702f0c22f99a560addd
Ralf Meyer <ralf.meyer@tugraz.at>
2017-11-09 11:58:09 +0100

    CHG: Expanded to check more behaviour

--- tests/test_for_leaks.cpp
+++ tests/test_for_leaks.cpp
@@ -11,16 +11,31 @@ int main()
   double* prms = new double[2];
   prms[0] = 0.0;
   prms[1] = 1.0;
-  double* prms3 = new double[3];
-  prms3[0] = 1.0;
-  prms3[1] = 1.0;
-  prms3[2] = 1.0;
+  double* prms3 = new double[3] {1.0, 1.0, 1.0};
   sfs->add_TwoBodySymmetryFunction(0, 0, 0, 2, prms, 0, 7.0);
+  sfs->add_TwoBodySymmetryFunction(0, 1, 0, 2, prms, 0, 7.0);
+  sfs->add_TwoBodySymmetryFunction(1, 0, 0, 2, prms, 0, 7.0);
+  sfs->add_TwoBodySymmetryFunction(1, 1, 0, 2, prms, 0, 7.0);
   sfs->add_ThreeBodySymmetryFunction(0, 0, 0, 0, 3, prms3, 0, 7.0);
+  sfs->add_ThreeBodySymmetryFunction(0, 0, 1, 0, 3, prms3, 0, 7.0);
   printf("SymFunSet created\n");
+
+  int num_atoms = 20;
+  int* types = new int[num_atoms]();
+  types[2] = 1;
+  types[3] = 1;
+
+  int G_size = sfs->get_G_vector_size(num_atoms, types);
+  double* out = new double[G_size]();
+  double* xyzs = new double[3*num_atoms]();
+
+  sfs->eval(num_atoms, types, xyzs, out);
 
   delete[] prms;
   delete[] prms3;
+  delete[] types;
+  delete[] out;
+  delete[] xyzs;
   delete sfs;
   return 0;
 }
