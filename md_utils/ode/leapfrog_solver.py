0a9a9243b68e58a2ef9bd02f389f943b69934baf
Johannes Cartus <cartus@student.tugraz.at>
2017-11-17 13:30:50 +0100

    CHG: first commit on purspose (actually initial commit)

--- md_utils/ode/leapfrog_solver.py
+++ md_utils/ode/leapfrog_solver.py
@@ -56,10 +56,8 @@ class LeapfrogSolverLangevin(os.OdeSolver):
         super(LeapfrogSolverLangevin, self).__init__(force, p_set, dt)
 
         # used as buffer for old acceleration (Langevin modified)
-        self.__AiLangevin = np.zeros(self.force.A.shape)
-        #self.__thermo = thermostats.BerensdenNVT(p_set, dt,
-        #                                         p_set.thermostat_coupling_time,
-        #                                         p_set.thermostat_temperature)
+        self.__Ai = _np.zeros(self.force.A.shape)
+
 
     def langevin_force(self, force, p_set):
         """The Lagevin version of the force model: keeps the sysem at
@@ -76,8 +74,16 @@ class LeapfrogSolverLangevin(os.OdeSolver):
         See also: https://en.wikipedia.org/wiki/Langevin_dynamics
         """
 
-        R = np.random.rand(*force.shape) - 0.5
-        F_random = np.sqrt(2 * self._gamma * k_B * self._temperature) * R
+        # conversion from J to system mass and length (time should still be seconds!!!)
+        unit_conversion = p_set.mass_unit * p_set.unit ** 2
+
+        # apply random force normally distributed around 0, var= 2gammak_BT
+        F_random = _np.random.normal(
+            0,
+            _np.sqrt(2 * self._gamma * unit_conversion * k_B * self._temperature),
+            force.shape
+        )
+
 
         F_friction = - self._gamma * p_set.V
 
@@ -98,20 +104,28 @@ class LeapfrogSolverLangevin(os.OdeSolver):
         """
 
         # store current foce (Langevin manipulated)
-        self.__AiLangevin[:] = self.langevin_force(self.force.A, self.pset)
+        self.__Ai[:] = self.langevin_force(self.force.A, self.pset)
 
         # calculate r(t + dt)
         self.pset.X[:] = \
             self.pset.X + self.pset.V * dt + 0.5 * self.force.A * dt ** 2.0
 
+
+
         # calculate f(t + dt)
         self.force.update_force(self.pset)
+
+        if hasattr(self.force, 'E'):
+            self.all_energies.append(self.force.E)
+        self.all_forces.append(self.force.F)
 
         # calculate v(t + dt)
         self.pset.V[:] = \
             self.pset.V + 0.5 * (
-                self.__AiLangevin +
+                self.__Ai +
                 self.langevin_force(self.force.A, self.pset)
             ) * dt
+
+        #print(self.pset.V[:])
 
         self.pset.update_boundary()
