CHG:Fixed force calculation for partitioned networks

NeuralNetworks/check_pes.py
@@ -13,10 +13,10 @@ class PES(object):
         self.geoms=[]
         self.figure=plt.figure()
         self.ax = Axes3D(self.figure)
-        self.ax.set_zlim(bottom=-1,top=5)
+        #self.ax.set_zlim(bottom=-1,top=5)
         plt.title(model_name)
         self.resolution=resolution
-        self.theta=np.linspace(0.1*np.pi/180,360*np.pi/180,self.resolution)
+        self.theta=np.linspace(1*np.pi/180,359*np.pi/180,self.resolution)
         self.rhs=np.linspace(1,7,self.resolution)
         self.meshX,self.meshY=np.meshgrid(self.rhs,self.theta*180/np.pi)
         meshZ=np.zeros((len(self.theta),len(self.rhs)))
@@ -82,7 +82,7 @@ class PES(object):
 
 if __name__ == "__main__":
     plt.ioff()
-    file="/home/afuchs/Documents/NiAu_Training_smaller_cutoff"
+    file="/home/afuchs/Documents/NiAu_Training_part/1/"
     #file="/home/afuchs/Documents/NiAu_Training/NiAu_Comp/without_4"
     MyCheck1=PES(file)
     MyCheck1.pes_check(atom_types=["X","Y"],nr_atoms_per_type=[1,2],show_plot=False)

examples/MD_Run.py
@@ -30,7 +30,7 @@ start_geom=[('Ni', np.array([-0.03069231,  0.077     , -0.00761538])),
 Training=NeuralNetworkUtilities.AtomicNeuralNetInstance()
 Training.CalcDatasetStatistics=False
 Training.TextOutput=False
-Training.prepare_evaluation("/home/afuchs/Documents/NiAu_Training_smaller_cutoff",nr_atoms_per_type=[1,12])
+Training.prepare_evaluation("/home/afuchs/Documents/NiAu_Training_part/1/",nr_atoms_per_type=[1,12])
 
 opt=optimizer.Optimizer(Training,start_geom)
 #opt.check_gradient()
@@ -43,9 +43,9 @@ input_reader.read("/home/afuchs/Documents/Validation_geometries/ico_NiAu54.xyz",
 Training=NeuralNetworkUtilities.AtomicNeuralNetInstance()
 Training.TextOutput=False
 Training.CalcDatasetStatistics=False
-Training.prepare_evaluation("/home/afuchs/Documents/NiAu_Training_smaller_cutoff",nr_atoms_per_type=[1,12])
+Training.prepare_evaluation("/home/afuchs/Documents/NiAu_Training_part/1/",nr_atoms_per_type=[1,12])
 
-dt = 1e-14
+dt = 5e-15
 steps = 50000
 
 
@@ -82,7 +82,7 @@ NNForce.set_masses(pset.M)
 NNForce.update_force(pset)
 solver = svs.LeapfrogSolverBerendsen( NNForce , pset , dt )
 solver.plot=True
-solver.plot_steps=5000
+solver.plot_steps=500
 solver.steps=steps
 solver.save_png=True
 solver.png_path="/home/afuchs/Documents/NiAu_Md/Ni1Au12_hot/"